dd056de6dfe6baa7f890e262a2eeab4cffa17a9a
devhassan703 <devhassan703@gmail.com>
2024-12-09 17:38:36 +0500

    Lesson 1

---
+++ lesson_1( JS basics)/variables.js
@@ -0,0 +1,13 @@
+// variables are used to store the values
+
+// declare the variable
+var firstName = "Hassan Faisal"
+
+// print the varibale values
+console.log("delacred variable value: "+firstName)
+
+// we can change the variables values or update values
+
+firstName = "Maham Ameer"
+
+console.log("update variable value: "+firstName)
